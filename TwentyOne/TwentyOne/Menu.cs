c4754b70c3b6ac2789e411530fb2597fe7adecb6
DESKTOP-HDIP5R8\nowot <nowotny.kuba@gmail.com>
2020-12-05 13:25:31 +0100

    ShowMenu function upadate

--- TwentyOne/TwentyOne/Menu.cs
+++ TwentyOne/TwentyOne/Menu.cs
@@ -8,7 +8,14 @@ namespace TwentyOne
     {
         public static void ShowMenu()
         {
-            Console.WriteLine("Hello in Twenty-One simulator.");
+            Console.WriteLine("Welcome in Twenty-One simulator.");
+            Console.WriteLine("This appliaction will allow you to calculate your chances of getting 21.");
+            Console.WriteLine("Legend:");
+            Console.WriteLine("For the card value type: Ace, 2, 3, ..., 10, Jack, Queen, King");
+            Console.WriteLine("For Clubs type: C number");
+            Console.WriteLine("For Diamonds type: D number");
+            Console.WriteLine("For Hearts type: H number");
+            Console.WriteLine("For Spades type: S number");
         }
     }
 }
